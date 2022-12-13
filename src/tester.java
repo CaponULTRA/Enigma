593a326d67d2a333e9970670d3c774d7e7326de7
CaponULTRA <fabalexrog@gmail.com>
2022-12-14 00:47:02 +0100

    Ready

--- src/tester.java
+++ src/tester.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class tester {
     public static void main(String[] args) {
-        new tester(1);
+        new tester(2);
     }
     public tester(int seed){
 
@@ -14,16 +14,18 @@ public class tester {
             arrayList.add((char)(i + 65));
         }
         for (int i = 0; i < 26;i++){
-            int place = random.nextInt();
-            do {place = random.nextInt((arrayList.size()-1));}while (arrayList.get(place)==(char)(i+65));
+
             while (chars[i]!='\0'){
                 i++;
-            }
-            while (!arrayList.isEmpty())   {
+            }int place = random.nextInt((arrayList.size()-1)-1) ;
+            do {place = random.nextInt((arrayList.size()-1)-1 muss);}while (arrayList.get(place)==(char)(i+65));
+
+            if (!arrayList.isEmpty())   {
                 chars[i]=arrayList.get(place);
                 chars[(arrayList.get(place)-65)]=(char) (i+65);
-                arrayList.remove((char) (i + 65));
                 arrayList.remove(place);
+                arrayList.remove(arrayList.indexOf((char) (i + 65)));
+
 
             }
 

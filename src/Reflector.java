bd7105e48e20ced735727fdff9ff43453af15147
CaponULTRA <fabalexrog@gmail.com>
2022-12-14 07:54:01 +0100

    Ready

--- src/Reflector.java
+++ src/Reflector.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Reflector {
 
@@ -10,9 +12,43 @@ public class Reflector {
             case ('A') -> exchange = reflectorA.toCharArray();
             case ('B') -> exchange = reflectorB.toCharArray();
             case ('C') -> exchange = reflectorC.toCharArray();
+
         }
+    }
+    public Reflector(int type){
+        exchange = random(type).toCharArray();
     }
     public char change(char input){
         return exchange[(int)input-65];
+    }
+    public String random(int seed){
+
+        Random random = new Random(seed);
+        char [] chars = new char[26];
+        ArrayList<Character> arrayList = new ArrayList<Character>();
+        for (int i = 0; i < 26;i++ ){
+            arrayList.add((char)(i + 65));
+        }
+        for (int i = 0; i < 26;i++){
+            if (!arrayList.isEmpty())   {
+                while (chars[i]!='\0'){
+                    i++;
+                }int place  ;
+                do {place = random.nextInt(0,(arrayList.size()) );
+                    System.out.println(place);}while (arrayList.get(place)==(char)(i+65));
+
+
+                chars[i]=arrayList.get(place);
+                chars[(arrayList.get(place)-65)]=(char) (i+65);
+                arrayList.remove(place);
+                arrayList.remove(arrayList.indexOf((char) (i + 65)));
+
+
+            }
+
+        }
+
+        return String.valueOf(chars);
+
     }
 }
--- src/tester.java
+++ src/tester.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class tester {
     public static void main(String[] args) {
-        new tester(2);
+        new tester(3);
     }
     public tester(int seed){
 
@@ -14,13 +14,14 @@ public class tester {
             arrayList.add((char)(i + 65));
         }
         for (int i = 0; i < 26;i++){
-
+            if (!arrayList.isEmpty())   {
             while (chars[i]!='\0'){
                 i++;
-            }int place = random.nextInt((arrayList.size()-1)-1) ;
-            do {place = random.nextInt((arrayList.size()-1)-1 muss);}while (arrayList.get(place)==(char)(i+65));
+            }int place  ;
+            do {place = random.nextInt(0,(arrayList.size()) );
+                System.out.println(place);}while (arrayList.get(place)==(char)(i+65));
 
-            if (!arrayList.isEmpty())   {
+
                 chars[i]=arrayList.get(place);
                 chars[(arrayList.get(place)-65)]=(char) (i+65);
                 arrayList.remove(place);
